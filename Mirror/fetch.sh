6b51d7a15655f344d7fdcca7d5cd4172a84af50d
adroitadorkhan <adroitadorkhan@gmail.com>
2018-06-25 01:43:21 -0400

    Auto Repo Cleanup.

--- Mirror/fetch.sh
+++ Mirror/fetch.sh
@@ -5,7 +5,7 @@ MOAAB=https://raw.githubusercontent.com/Ultimate-Hosts-Blacklist/adblock.mahakal
 AMOAAB=https://raw.githubusercontent.com/Ultimate-Hosts-Blacklist/adblock.mahakala.is/master/output/hosts/ACTIVE/hosts
 SEX=https://raw.githubusercontent.com/Ultimate-Hosts-Blacklist/Airelles_Anti_Sex_Hosts/master/clean.list
 ASEX=https://raw.githubusercontent.com/Ultimate-Hosts-Blacklist/Airelles_Anti_Sex_Hosts/master/output/hosts/ACTIVE/hosts
-# Fetch latest Moaab
+# Fetch latest MoaAB
 wget -O MoaAB/MoaAB.txt $MOAAB
 # Fetch clean MoaAB List
 wget -O MoaAB/MoaAB.active.txt $AMOAAB
